5328b931053fc21b0833931c2934c797469bafe6
github username <github email address>
2018-10-11 16:53:28 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,22 @@
+class Backer
+
+	attr_accessor :backed_projects, :name, :project
+
+	def initialize(name)
+		@name = name
+		@backed_projects = []
+	end
+#More Advanced #back_project also adds
+#the backer to the project's backers array
+	def back_project(project)
+
+		@backed_projects << project
+
+
+		project.backers << self
+
+
+
+
+	end
+end
