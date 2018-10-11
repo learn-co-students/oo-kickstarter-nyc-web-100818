4314096aea175554be86fb63fb78ef7d5e6af78b
Rexman17 <a.stone1794@gmail.com>
2018-10-11 15:31:32 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_accessor :title, :backers, :name
+
+  def initialize(title)
+    @title = title
+    @backers = [] # push instance of Backer class into this
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self 
+  end
+  # also adds the project to the backer's backed_projects array
+
+end
