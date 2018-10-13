a8c68f88b18ddb482cde3ca21ccf730005a79564
github username <github email address>
2018-10-13 12:06:06 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,71 @@
 class Backer
-  attr_accessor :backed_projects, :name
-  def initialize(name)
-    @name = name
-    @backed_projects = []
-  end
-  def back_project(project)
-    @backed_projects << project
-    project.backers << self
-  end
+
+attr_accessor :name, :backed_projects
+
+
+def initialize(name)
+  @name = name
+  @backed_projects = []
 end
+
+
+
+#bob.back_project(awesome_project)
+def back_project(project)
+  @backed_projects << project
+  project.backers << self
+end
+
+
+end
+
+
+
+# Backer - More Advanced #back_project
+#   also adds the backer to the project's backers array (FAILED - 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Backer
+#   attr_accessor :backed_projects, :name
+#   def initialize(name)
+#     @name = name
+#     @backed_projects = []
+#   end
+#   def back_project(project)
+#     @backed_projects << project
+#     project.backers << self
+#   end
+# end
--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,69 @@
 class Project
-  attr_accessor :backers, :title
+
+  attr_accessor :title, :backers
+
   def initialize(title)
     @title = title
     @backers = []
+
   end
-  def add_backer(backer)
-    @backers << backer
-    backer.backed_projects << self
-  end
+
+
+def add_backer(backer)
+  @backers << backer
+  backer.backed_projects << self  #calling backer(project) to be put into backed_projects(backer array) 
 end
+
+end
+
+#
+# Project - More Advanced #add_backer
+#   also adds the project to the backer's backed_projects array (FAILED - 1)
+# answer : backer.backed_projects << self
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Project
+#   attr_accessor :backers, :title
+#   def initialize(title)
+#     @title = title
+#     @backers = []
+#   end
+#   def add_backer(backer)
+#     @backers << backer
+#     backer.backed_projects << self
+#   end
+# end
